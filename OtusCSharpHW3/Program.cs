80556fe2c2e7799862fef831f61b0e5aeab54ae6
softline\konovalovvd <Vadim.Konovalov@softline.com>
2026-02-07 14:56:34 +0300

    Добавьте файлы проекта.

---
+++ OtusCSharpHW3/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace OtusCSharpHW3
+{
+    public class SimpleStore
+    {
+        private Dictionary<string, byte[]> _store;
+
+        public SimpleStore()
+        {
+            _store=new Dictionary<string, byte[]>();
+        }
+        public void Set(string key, byte[] value)
+        {
+            _store[key]= value;
+        }
+        public byte[] Get(string key)
+        {
+            _store.TryGetValue(key, out var value);
+            return value;
+        }
+        public void Delete(string key)
+        {
+            _store.Remove(key);
+        }
+    }
+
+    public ref struct CommandKeyValue
+    {
+        public ReadOnlySpan<string> Command { init; get; }
+        public ReadOnlySpan<string> Key { init; get; }
+        public ReadOnlySpan<string> Value { init; get; }
+    }
+
+    public static class CommandParser
+    {
+        public static CommandKeyValue Parse(ReadOnlySpan<string> strings)
+        {
+            return new CommandKeyValue()
+            {
+                Command =
+            };
+        }
+    }
+    internal class ProgramHW3
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            SimpleStore simple = new SimpleStore();
+            simple.Get("xz");
+        }
+    }
+}
---
+++ Tests/HW3Tests.cs
@@ -0,0 +1,11 @@
+namespace Tests
+{
+    public class HW3Tests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
