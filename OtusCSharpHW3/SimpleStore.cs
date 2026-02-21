092a8f9974dcb2d7179801c9334f5bbfa0bd64eb
softline\konovalovvd <Vadim.Konovalov@softline.com>
2026-02-22 02:44:36 +0300

    Потокобезопасное хранилище и сбор статистики

---
+++ OtusCSharpHW3/SimpleStore.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OtusCSharpHW3
+{
+    public class SimpleStore : IDisposable
+    {
+        private Dictionary<string, byte[]> _store;
+        private readonly ReaderWriterLockSlim _lock = new ReaderWriterLockSlim();
+        private long _setCount, _getCount, _deleteCount;
+
+        public SimpleStore()
+        {
+            _store = new Dictionary<string, byte[]>();
+        }
+
+        public(long,long,long) GetStatistics()
+        {
+            return (_setCount, _getCount, _deleteCount);
+        }
+        public void Set(string key, byte[] value)
+        {
+            _lock.EnterWriteLock();
+            try
+            {
+                _store[key] = value;
+            }
+            finally
+            {
+                Interlocked.Increment(ref _setCount);
+                _lock.ExitWriteLock();
+            }
+        }
+        public byte[] Get(string key)
+        {
+            _lock.EnterReadLock();
+            try
+            {
+                _store.TryGetValue(key, out var value);
+                return value;
+            }
+            finally
+            {
+                Interlocked.Increment(ref _getCount);
+                _lock.ExitReadLock();
+            }
+            
+        }
+        public void Delete(string key)
+        {
+            _lock.EnterWriteLock();
+            try
+            {
+                _store.Remove(key);
+            }
+            finally
+            {
+                Interlocked.Increment(ref _deleteCount);
+                _lock.ExitWriteLock();
+            }
+        }
+        public void Dispose()
+        {
+            _lock.Dispose(); 
+        }
+    }
+
+}
---
+++ Tests/SimpleStoreTests.cs
@@ -0,0 +1,57 @@
+﻿namespace Tests;
+using OtusCSharpHW3;
+
+public class SimpleStoreTests
+{
+    [Fact]
+    public void MultiTaskTest()
+    {
+        using SimpleStore simpleStore = new SimpleStore();
+        Task task1 = Task.Run(() =>
+        {
+            simpleStore.Set("3", new byte[2] { 0, 1 });
+            simpleStore.Get("1");
+        });
+
+        Task task2 = Task.Run(() =>
+        {
+            simpleStore.Set("2", new byte[1] { 4 });
+            simpleStore.Get("1");
+            simpleStore.Delete("1");
+        });
+
+        Task task3 = Task.Run(() =>
+        {
+            simpleStore.Set("1", new byte[2] { 0, 1 });
+
+            var bytes = simpleStore.Get("1");
+           
+            Assert.NotNull(bytes);
+            Assert.Equal(1, bytes[1]);
+
+            simpleStore.Delete("1");
+            bytes = simpleStore.Get("1");
+            Assert.Null(bytes);
+
+            simpleStore.Delete("3");
+        });
+
+        Task task4 = Task.Run(() =>
+        {
+            simpleStore.Set("3", new byte[2] { 1, 0 });
+            simpleStore.Get("3");
+            
+        });
+
+        Task.WaitAll(new Task[4] { task1, task2, task3, task4 });
+
+        var statistics = simpleStore.GetStatistics();
+        Assert.Equal(4, statistics.Item1);
+        Assert.Equal(5, statistics.Item2);
+        Assert.Equal(3, statistics.Item3);
+
+
+
+    }
+   
+}
